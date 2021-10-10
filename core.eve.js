fab894f55c8c9804274e19d9d288b935a528a406
Silvan Buedenbender <silvancodes@gmail.com>
2021-10-10 22:56:05 +0200

    Use ES6 class syntax

--- core.eve.js
+++ core.eve.js
@@ -1,20 +1,24 @@
-function Eve () {
+class Eve {
+  constructor() {
     this.eventMap = new Map();
-}
-
-Eve.prototype = {
-    constructor: Eve,
-    on(event, fn) {
-        (this.eventMap.has(event) ? this.eventMap : this.eventMap.set(event, new Set())).get(event).add(fn);
-    },
-    off(event, fn) {
-        this.eventMap.has(event) && (fn ? this.eventMap.get(event).delete(fn) : this.eventMap.delete(event));
-    },
-    emit(event, ...data)  {
-        const fns = this.eventMap.get(event);
-        if (!fns) return;
-        for (const fn of fns) {
-            fn(...data);
-        }
+  }
+  on(event, fn) {
+    (this.eventMap.has(event)
+      ? this.eventMap
+      : this.eventMap.set(event, new Set())
+    )
+      .get(event)
+      .add(fn);
+  }
+  off(event, fn) {
+    this.eventMap.has(event) &&
+      (fn ? this.eventMap.get(event).delete(fn) : this.eventMap.delete(event));
+  }
+  emit(event, ...data) {
+    const fns = this.eventMap.get(event);
+    if (!fns) return;
+    for (const fn of fns) {
+      fn(...data);
     }
+  }
 }
